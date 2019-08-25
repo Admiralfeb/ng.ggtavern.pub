6557cd650cf0a61101683be3c5842e21dedf8ae9
Zachary Bryant <Zachary@admiralfeb.net>
2019-08-24 21:51:29 -0500

    home created

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   title = 'Grinning Goblin Gaming Tavern';
+  offerings = [
+    { id: 'item0', text: 'Retro Video Games' },
+    { id: 'item1', text: 'Card Games' },
+    { id: 'item2', text: 'Food' },
+    { id: 'item3', text: 'Board Games' },
+    { id: 'item4', text: 'Beer' },
+    { id: 'item5', text: 'Virtual Reality' },
+    { id: 'item6', text: 'Liquor' }
+  ];
   constructor() { }
 
   ngOnInit() {
