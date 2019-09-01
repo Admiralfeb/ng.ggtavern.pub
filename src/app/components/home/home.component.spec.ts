1e300c3f595d6b1f54b4d54e27bf344d2fe20805
Zachary Bryant <Zachary@admiralfeb.net>
2019-09-01 14:30:05 -0500

    spec test correction

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { OpenSignComponent } from '../open-sign/open-sign.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -9,7 +10,10 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent],
+      declarations: [
+        HomeComponent,
+        OpenSignComponent
+      ],
       imports: [RouterTestingModule]
     })
       .compileComponents();
