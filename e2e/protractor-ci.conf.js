e9cc165ccc6a1214b44c2417ecf239ed7dcf3a05
Zachary Bryant <Zachary@admiralfeb.net>
2019-09-07 02:04:09 -0500

    add tests to pipeline

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -1,11 +1,40 @@
 const config = require('./protractor.conf').config;
+const junitReporter = require('jasmine-reporters').JUnitXmlReporter;
 
-config.capabilities = {
-    browserName: 'chrome',
-    chromeOptions: {
-        binary: require('puppeteer').executablePath(),
-        args: ['--headless', '--no-sandbox']
+const { SpecReporter } = require('jasmine-spec-reporter');
+
+/**
+ * @type { import("protractor").Config }
+ */
+exports.config = {
+    allScriptsTimeout: 11000,
+    specs: [
+        './src/**/*.e2e-spec.ts'
+    ],
+    capabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+            binary: require('puppeteer').executablePath(),
+            args: ['--headless', '--no-sandbox']
+        }
+    },
+    directConnect: true,
+    baseUrl: 'http://localhost:4200/',
+    framework: 'jasmine',
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000,
+        print: function () { }
+    },
+    onPrepare() {
+        require('ts-node').register({
+            project: require('path').join(__dirname, './tsconfig.json')
+        });
+        jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+        var junitReporter = new JUnitXmlReporter({
+            savePath: require('path').join(__dirname, './tests/e2e'),
+            consolidateAll: true
+        });
+        jasmine.getEnv().addReporter(junitReporter);
     }
 };
-
-exports.config = config;
