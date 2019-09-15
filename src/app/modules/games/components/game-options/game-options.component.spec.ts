9a6e2a26b96fee340e4c4e4343171a0084b81f8f
Zachary Bryant <Zachary@admiralfeb.net>
2019-09-15 17:50:53 -0500

    correction

--- src/app/modules/games/components/game-options/game-options.component.spec.ts
+++ src/app/modules/games/components/game-options/game-options.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { GameOptionsComponent } from './game-options.component';
 import { SharedModule } from '@shared/shared-module.module';
