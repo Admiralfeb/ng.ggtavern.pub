17cbe6aa9eb30ae5131f32e3dd5d79282f0a1470
Zachary Bryant <Zachary@admiralfeb.net>
2019-12-05 21:51:24 -0600

    test: fix about-main component tests

--- src/app/modules/about/components/about-main/about-main.component.spec.ts
+++ src/app/modules/about/components/about-main/about-main.component.spec.ts
@@ -1,16 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutMainComponent } from './about-main.component';
+import { SharedModule } from '@shared/shared-module.module';
+import { AboutService } from '../../services/about.service';
 
 describe('AboutMainComponent', () => {
   let component: AboutMainComponent;
   let fixture: ComponentFixture<AboutMainComponent>;
 
   beforeEach(async(() => {
+    const aboutServiceStub = jasmine.createSpyObj<AboutService>('AboutService', ['getPositions']);
+
     TestBed.configureTestingModule({
-      declarations: [ AboutMainComponent ]
+      declarations: [AboutMainComponent],
+      imports: [SharedModule],
+      providers: [
+        { provide: AboutService, useValue: aboutServiceStub }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
