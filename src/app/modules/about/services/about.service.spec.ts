6ae0facdc2ceaf9e8882c4a59a13f978ec086703
Zachary Bryant <Zachary@admiralfeb.net>
2019-12-07 00:02:53 -0600

    test: fix about service test init

--- src/app/modules/about/services/about.service.spec.ts
+++ src/app/modules/about/services/about.service.spec.ts
@@ -6,9 +6,11 @@ describe('AboutService', () => {
   let service: AboutService;
 
   const mockDatabaseService = jasmine.createSpyObj<DatabaseService>('DatabaseService', ['getItems']);
+  const sanitizer = jasmine.createSpyObj('DomSanitizer', ['bypass']);
+
 
   beforeEach(() => {
-    service = new AboutService(mockDatabaseService);
+    service = new AboutService(mockDatabaseService, sanitizer);
   });
 
   it('should be created', () => {
