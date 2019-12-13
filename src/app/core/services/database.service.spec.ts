9830f87f998ab5eec0f824395ab534073a607cd7
Zachary Bryant <Zachary@admiralfeb.net>
2019-12-12 20:56:25 -0600

    test(database.service): add tests

--- src/app/core/services/database.service.spec.ts
+++ src/app/core/services/database.service.spec.ts
@@ -2,12 +2,10 @@ import { DatabaseService } from './database.service';
 import { AngularFirestore, Query } from '@angular/fire/firestore';
 import { TestBed } from '@angular/core/testing';
 import firebase from 'firebase';
-import { Observable, of } from 'rxjs';
+import { of } from 'rxjs';
 
 describe('DatabaseService', () => {
   let service: DatabaseService;
-
-
   const sortTestData: TestData[] = [
     {
       test: 'objects-c',
@@ -44,110 +42,19 @@ describe('DatabaseService', () => {
       ]
     }
   ];
-  const blankData: firebase.firestore.QuerySnapshot = {
-    docs: [],
-    metadata: null,
-    size: null,
-    empty: true,
-    query: null,
-    docChanges: null,
-    forEach: null,
-    isEqual: null
-  };
-  const someData: firebase.firestore.QuerySnapshot = {
-    docs: [
-      {
-        data: () => sortTestData[0],
-        exists: true,
-        ref: null,
-        id: null,
-        metadata: null,
-        get: null,
-        isEqual: null
-      },
-      {
-        data: () => sortTestData[1],
-        exists: true,
-        ref: null,
-        id: null,
-        metadata: null,
-        get: null,
-        isEqual: null
-      },
-      {
-        data: () => sortTestData[2],
-        exists: true,
-        ref: null,
-        id: null,
-        metadata: null,
-        get: null,
-        isEqual: null
-      }
-    ],
-    metadata: null,
-    size: null,
-    empty: false,
-    query: null,
-    docChanges: null,
-    forEach: null,
-    isEqual: null
-  };
-
-  const blankObservable = of(blankData);
-  const someObservable = of(someData);
-
-  const someSpy = {
-    get: jasmine.createSpy('get').and.returnValue(someObservable)
-  };
-  const firestoreSomeSpy = {
-    collection: jasmine.createSpy('collection').and.returnValue(someSpy)
-  };
-
-  const blankSpy = {
-    get: jasmine.createSpy('get').and.returnValue(blankObservable)
-  };
-  const firestoreBlankSpy = {
-    collection: jasmine.createSpy('collection').and.returnValue(blankSpy)
-  };
-
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        { provide: AngularFirestore, useValue: firestoreSomeSpy },
+        { provide: AngularFirestore, useValue: {} },
       ]
     });
     service = TestBed.get(DatabaseService);
   });
 
-  afterEach(() => {
-  });
-
-
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-
-  it('should get an empty array when collection is empty', async (done) => {
-    TestBed.overrideProvider(AngularFirestore, { useValue: firestoreBlankSpy });
-    service = TestBed.get(DatabaseService);
-    const items = await service.getItems('');
-    expect(items).toEqual([]);
-    done();
-  });
-
-  it('should get an array of test items', async (done) => {
-    TestBed.overrideProvider(AngularFirestore, { useValue: firestoreSomeSpy });
-    service = TestBed.get(DatabaseService);
-
-    const items = await service.getItems('');
-    console.log({ items });
-    done();
-  });
-
-
-
-
 
   sortTestData.forEach(test => {
     it(`should sort ${test.test}`, () => {
@@ -160,6 +67,155 @@ describe('DatabaseService', () => {
   it('should not sort incongruous items', () => {
     const returned = service.sortItems([{ twi: 'whoo' }, { la: 'yay' }, { bob: 'not a god' }], 'bob');
     expect(returned).toEqual([{ twi: 'whoo' }, { la: 'yay' }, { bob: 'not a god' }]);
+  });
+
+  it('should return items on failed sort', () => {
+    spyOn(Array.prototype, 'sort').and.throwError('oops');
+    const items = service.sortItems([1, 2, 3], null);
+    expect(items).toEqual([1, 2, 3]);
+  });
+
+});
+
+describe('DatabaseService calls to FireStore', () => {
+  let service: DatabaseService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: AngularFirestore, useValue: {} },
+      ]
+    });
+    // service = TestBed.get(DatabaseService);
+  });
+
+  it('should get an empty array when collection is empty', async () => {
+    const blankData: firebase.firestore.QuerySnapshot = {
+      docs: [],
+      metadata: null,
+      size: null,
+      empty: true,
+      query: null,
+      docChanges: null,
+      forEach: null,
+      isEqual: null
+    };
+    const blankObservable = of(blankData);
+    const blankSpy = {
+      get: jasmine.createSpy('get').and.returnValue(blankObservable)
+    };
+    const firestoreBlankSpy = {
+      collection: jasmine.createSpy('collection').and.returnValue(blankSpy)
+    };
+
+    TestBed.overrideProvider(AngularFirestore, { useValue: firestoreBlankSpy });
+    service = TestBed.get(DatabaseService);
+
+    const items = await service.getItems('');
+    expect(items).toEqual([]);
+  });
+
+  it('should get an array of test items', async () => {
+    const singleTestData: TestData[] = [
+      {
+        test: 'objects-c',
+        data: [
+          { a: 'hello', b: 'really', c: 'fun' },
+          { a: 'bye', b: 'ah', c: 'zed' }],
+        sort: 'c',
+        expected: [
+          { a: 'hello', b: 'really', c: 'fun' },
+          { a: 'bye', b: 'ah', c: 'zed' }
+        ]
+      },
+      {
+        test: 'objects-b',
+        data: [
+          { a: 'hello', b: 'really', c: 'fun' },
+          { a: 'bye', b: 'ah', c: 'zed' }
+        ],
+        sort: 'b',
+        expected: [
+          { a: 'bye', b: 'ah', c: 'zed' },
+          { a: 'hello', b: 'really', c: 'fun' }]
+      },
+      {
+        test: 'objects-a',
+        data: [
+          { a: 'hello', b: 'really', c: 'fun' },
+          { a: 'bye', b: 'ah', c: 'zed' }
+        ],
+        sort: 'a',
+        expected: [
+          { a: 'bye', b: 'ah', c: 'zed' },
+          { a: 'hello', b: 'really', c: 'fun' }
+        ]
+      }
+    ];
+    const someData: firebase.firestore.QuerySnapshot = {
+      docs: [
+        {
+          data: () => singleTestData[0],
+          exists: true,
+          ref: null,
+          id: null,
+          metadata: null,
+          get: null,
+          isEqual: null
+        },
+        {
+          data: () => singleTestData[1],
+          exists: true,
+          ref: null,
+          id: null,
+          metadata: null,
+          get: null,
+          isEqual: null
+        },
+        {
+          data: () => singleTestData[2],
+          exists: true,
+          ref: null,
+          id: null,
+          metadata: null,
+          get: null,
+          isEqual: null
+        }
+      ],
+      metadata: null,
+      size: null,
+      empty: false,
+      query: null,
+      docChanges: null,
+      forEach: null,
+      isEqual: null
+    };
+    const someObservable = of(someData);
+    const someSpy = {
+      get: jasmine.createSpy('get').and.returnValue(someObservable)
+    };
+    const firestoreSomeSpy = {
+      collection: jasmine.createSpy('collection').and.returnValue(someSpy)
+    };
+
+    TestBed.overrideProvider(AngularFirestore, { useValue: firestoreSomeSpy });
+    service = TestBed.get(DatabaseService);
+
+    const items = await service.getItems('');
+    expect(items).toEqual(singleTestData);
+  });
+
+  it('should throw error if database errs', async () => {
+    const errorSpy = {
+      get: jasmine.createSpy('get').and.throwError('database failed')
+    };
+    const firestoreErrorSpy = {
+      collection: jasmine.createSpy('collection').and.returnValue(errorSpy)
+    };
+
+    TestBed.overrideProvider(AngularFirestore, { useValue: firestoreErrorSpy });
+    service = TestBed.get(DatabaseService);
+    await expectAsync(service.getItems<TestData>('')).toBeRejectedWithError('database failed');
   });
 
 });
