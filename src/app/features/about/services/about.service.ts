686b86647b45ccc895da43dfab90052f22677ebe
Zachary Bryant <Zachary@admiralfeb.net>
2019-12-14 18:56:44 -0600

    refactor: update about service and spec

--- src/app/features/about/services/about.service.ts
+++ src/app/features/about/services/about.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { Position, Worker } from '../about.models';
 import { DatabaseService } from '@core/services/database.service';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { SortService } from '@core/services/sort.service';
 
 @Injectable()
 export class AboutService {
 
-  constructor(private db: DatabaseService, private sanitizer: DomSanitizer) { }
+  constructor(private db: DatabaseService, private sort: SortService, private sanitizer: DomSanitizer) { }
 
   async getPositions(): Promise<Position[]> {
     const pathString = `positions`;
@@ -14,15 +15,15 @@ export class AboutService {
     try {
       // items = await this.db.getItems<T>(pathString);
       items = await this.db.getItemsandSingleSub<Position, Worker>(pathString, 'workers');
-      items = this.db.sortItems(items, 'order');
+      items = this.sort.sortItems(items, 'order');
       const fullItems = items.map(position => {
         if (position.workers) {
-          position.workers = this.db.sortItems(position.workers, 'name');
+          position.workers = this.sort.sortItems(position.workers, 'name');
           position.workers = position.workers.map(worker => {
             if (worker.imgPath) {
               worker.imgUrl = this.getImageUrl(worker.imgPath);
             } else {
-              worker.imgUrl = this.getImageUrl('../../../../../assets/img/android-chrome-512x512.png');
+              worker.imgUrl = this.getImageUrl('@assets/img/android-chrome-512x512.png');
             }
             return worker;
           });
@@ -37,16 +38,5 @@ export class AboutService {
 
   private getImageUrl(urlString: string): SafeUrl {
     return this.sanitizer.bypassSecurityTrustUrl(urlString);
-  }
-
-  private async getWorkers<T extends Worker>(position: string): Promise<T[]> {
-    const pathString = `positions/${position}/workers`;
-    let items: T[] = [];
-    try {
-      items = await this.db.getItems<T>(pathString);
-      return this.db.sortItems(items, 'name');
-    } catch (err) {
-      throw err;
-    }
   }
 }
