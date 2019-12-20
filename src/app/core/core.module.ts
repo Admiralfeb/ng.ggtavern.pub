342823b15273b38614343991b58d442d039ddea5
Zachary Bryant <Zachary@admiralfeb.net>
2019-12-20 16:19:42 -0600

    feat(calendar): add events to navbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,14 +2,16 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { EnsureModuleLoadedOnceGuard } from '@shared/ensureModuleLoadedOnce.guard';
 import { SharedModule } from '@shared/shared.module';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [NavbarComponent],
   imports: [
-    SharedModule
+    RouterModule,
+    SharedModule,
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
   ]
 })
 export class CoreModule extends EnsureModuleLoadedOnceGuard {
