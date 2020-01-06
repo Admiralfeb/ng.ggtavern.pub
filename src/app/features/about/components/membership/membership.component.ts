5b85afa0bb9053ebb4643dd8960bbfe4afdb2708
Zachary Bryant <Zachary@admiralfeb.net>
2020-01-05 18:21:03 -0600

    feat(about): add manifesto and membership pages

--- src/app/features/about/components/membership/membership.component.ts
+++ src/app/features/about/components/membership/membership.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MembershipComponent implements OnInit {
 
+  price = 25.00;
+  perks = [
+    'Free entry to all events unless otherwise stated',
+    '10% off all food and drink',
+    'Quest lines - win stuff!',
+    'Priority choice on consoles',
+    '1/2 price VR. Free VR on Sundays',
+    'Damage waiver only needs to be signed once a year',
+    'No need to leave an ID to rent games'
+  ];
+
   constructor() { }
 
   ngOnInit() {
