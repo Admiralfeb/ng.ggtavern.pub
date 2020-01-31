f8165964e2fd6ae189f362d6316606cefc63f638
Zachary Bryant <Zachary@admiralfeb.net>
2020-01-31 15:48:02 -0600

    build: configure project-level gulp tasks

--- gulpfile.ts/index.ts
+++ gulpfile.ts/index.ts
@@ -2,21 +2,18 @@ import gulp from 'gulp';
 import replace from 'gulp-replace';
 import path from 'path';
 const argv = require('minimist')(process.argv.slice(2));
-import shell from 'gulp-shell';
-import { exec } from 'child_process';
-
 
 require('./gulp.e2e');
 require('./gulp.test');
+require('./gulp.project');
 
-gulp.task('default', (done) => {
+gulp.task('default', done => {
     console.log('default');
     done();
 });
 
-gulp.task('postInstall', shell.task('ts-node scripts/generateKeyFile.ts'));
 
-gulp.task('bump-version', (done) => {
+gulp.task('bump-version', done => {
     const ngEnv = argv.configuration;
     const envDir = path.resolve(__dirname, `./src/environments`);
     const envFilePath = path.join(envDir, `environment.${ngEnv}.ts`);
@@ -42,22 +39,4 @@ gulp.task('bump-version', (done) => {
             })
         )
         .pipe(gulp.dest(envDir));
-});
-
-gulp.task('deploy', shell.task(`firebase deploy --only hosting`));
-
-gulp.task('get-version', () => {
-    return Promise.resolve(shell.task('node scripts/getversion'));
-});
-
-gulp.task('test-deploy', (done) => {
-    const pjson = require('../package.json');
-    const version: string = pjson.version;
-    exec(`firebase deploy --only hosting -m ${version}`, (err, stdout, stderr) => {
-        console.log(stdout.trim());
-        if (stderr) {
-            console.error({ stderr });
-        }
-        done();
-    });
 });
