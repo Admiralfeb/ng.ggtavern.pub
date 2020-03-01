6bc3762c23eba79482aaf38dbfdeb0811ee3ae69
Zachary Bryant <Zachary@admiralfeb.net>
2020-02-29 23:41:10 -0600

    feat(calendar): update day component for easier updates

--- src/app/features/calendar/components/day/day.component.ts
+++ src/app/features/calendar/components/day/day.component.ts
@@ -1,22 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { CalendarEvent } from '../../models/calendar-event.model';
+import { Component, Input } from '@angular/core';
+import { CalendarEvent } from '@features/calendar/models/calendar-event.model';
 
 @Component({
   selector: 'calendar-day',
   templateUrl: './day.component.html',
   styleUrls: ['./day.component.scss']
 })
-export class DayComponent implements OnInit {
+export class DayComponent {
   @Input() event: CalendarEvent;
-  date = 0;
-  eventName: string;
-  time: string;
   constructor() { }
-
-  ngOnInit() {
-    this.date = this.event.day;
-    this.eventName = this.event.eventName;
-    this.time = this.event.time;
-  }
-
 }
