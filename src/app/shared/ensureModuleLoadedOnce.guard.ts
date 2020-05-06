0b4760b06dd1fc02c804ffca1817bfe88933781e
Zachary Bryant <Zachary@admiralfeb.net>
2020-05-05 22:59:48 -0500

    build: add eslint

--- src/app/shared/ensureModuleLoadedOnce.guard.ts
+++ src/app/shared/ensureModuleLoadedOnce.guard.ts
@@ -2,14 +2,15 @@
  * Guards against loading a module multiple times.
  */
 export class EnsureModuleLoadedOnceGuard {
-    // tslint:disable: max-line-length
-    /**
-     * @param targetModule Should be keyed as `@Optional() @SkipSelf() parentModule: [ModuleName]` to allow search for the module this guards.
-     */
-    constructor(targetModule: any) {
-        // If the module is found to be already loaded, throw an error.
-        if (targetModule) {
-            throw new Error(`${targetModule.constructor.name} has already been loaded. Import this module in the AppModule only.`);
-        }
+  // tslint:disable: max-line-length
+  /**
+   * @param targetModule Should be keyed as `@Optional() @SkipSelf() parentModule: [ModuleName]`
+   * to allow search for the module this guards.
+   */
+  constructor(targetModule: any) {
+    // If the module is found to be already loaded, throw an error.
+    if (targetModule) {
+      throw new Error(`${targetModule.constructor.name} has already been loaded. Import this module in the AppModule only.`);
     }
+  }
 }
